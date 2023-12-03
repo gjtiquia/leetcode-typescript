be9913ba99882d0e88156f50f66106ecb16cc635
GJ Tiquia <gershom.tiquia@gmail.com>
2023-12-03 12:45:37 +0800

    feat: finish two sum

--- src/0001.TwoSum/twoSum.ts
+++ src/0001.TwoSum/twoSum.ts
@@ -1,3 +1,15 @@
 export function twoSum(nums: number[], target: number): number[] {
-    return [0];
+
+    for (var i = 0; i < nums.length; i++) {
+        const x = nums[i];
+
+        for (var j = i + 1; j < nums.length; j++) {
+            const y = nums[j];
+
+            if (x + y === target)
+                return [i, j];
+        }
+    }
+
+    throw new Error("No solution exists!");
 };
